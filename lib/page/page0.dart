685b8ffac1e69fdba1aecbbe309ecab023727b2d
Good Boy <Lawliet10478@gmail.com>
2022-04-28 15:44:09 +0700

    first

---
+++ lib/page/page0.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../widget/common/Checkbox.dart';
+
+bool isChecked = false;
+
+class Page0 extends StatelessWidget {
+  const Page0({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Page0Body();
+  }
+}
+
+class Page0Body extends StatelessWidget {
+  const Page0Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+          height: 100,
+          width: 200,
+          color: Colors.orange,
+          child: const Center(
+              child: Text("initial Page \nor do something wrong"))),
+    );
+  }
+}
